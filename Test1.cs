e74d5dcefa0d77caa706c0bd5bf43210875ae3eb
dennis-99 <dennis.mehner@student.jade-hs.de>
2019-10-04 21:00:12 +0200

    Update Test1.cs

--- Test1.cs
+++ Test1.cs
@@ -24,7 +24,7 @@ namespace Hello_World_Mecke
 
             // Ausgabe Benutzer 4
             Console.WriteLine("marhabaan bialealam");
-
+            Console.WriteLine("Brenne im ewigen Feuer der siebenten Ebene der Hölle");
             // warte bis der User eine Taste drueckt
             Console.ReadKey();
             //Brenne im ewigen Feuer der 7. Ebene der Hölle!!!
