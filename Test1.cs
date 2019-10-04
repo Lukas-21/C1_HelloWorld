adeebdca53003d25cfee2e89c55d58556b61cf7b
Dennis-99 <56160709+Dennis-99@users.noreply.github.com>
2019-10-04 15:10:29 +0200

    Update Test1.cs

--- Test1.cs
+++ Test1.cs
@@ -15,7 +15,7 @@ namespace Hello_World_Mecke
         {
             // Ausgabe Benutzer 1
             Console.WriteLine("Hello World von XXX");
-
+            //5678
             // Ausgabe Benutzer 2
             Console.WriteLine("Nǐ hǎo, shìjiè");
 
