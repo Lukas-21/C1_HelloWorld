e4ae3424fe64781097d693aec39f1a51a7cf7814
Colin2123 <56160072+Colin2123@users.noreply.github.com>
2019-10-11 18:46:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profil_Rechner_0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Grundprogramm zum testen der Variablen und zum Programmieren seines teil programmes.<3
+
+            //Ergebniss Variablen
+            String Volumen;
+            String Querschnitt;
+            String Gewicht;
+            String FTM;
+
+            //Rechen Operanden
+            String Hoehe;
+            String Laenge;
+            String Breite;
+            String Wandstaerke;
+
+            //Abfrage der Werte 
+            //Nur zum Testen ihr Keckos!!!!11!!1!
+            Console.WriteLine("Berechnung eines Rechteckprofils");
+
+            Console.WriteLine("Bitte die Gewuenschte Hoehe Eingeben");
+            Hoehe = Console.ReadLine();
+           
+            Console.WriteLine("Bitte die Gewuenschte Breite Eingeben");
+            Breite = Console.ReadLine();
+
+            Console.WriteLine("Bitte die Gewuenschte Laenge Eingeben");
+            Laenge = Console.ReadLine();
+
+            Console.WriteLine("Bitte die Gewuenschte Wandstaerke Eingeben");
+            Wandstaerke = Console.ReadLine();
+
+            Console.WriteLine("Hoehe");
+            Console.WriteLine(Hoehe);
+            Console.WriteLine("Breite");
+            Console.WriteLine(Breite);
+            Console.WriteLine("Laenge");
+            Console.WriteLine(Laenge);
+            Console.WriteLine("Wandstaerke");
+            Console.WriteLine(Wandstaerke);
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+              
+        }
+    }
+}
